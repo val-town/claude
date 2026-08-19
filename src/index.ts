011c88e30a605b6bf453a30a8caaed1566b1d146
Tom MacWright <tom@macwright.com>
2026-08-19 15:35:57 -0400

    Add parseSkill and scoreSkill

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export type { Skill, SkillSearchResult } from "./types.js";
 export { getSkills, searchSkills } from "./search.js";
 export { skills, skillList } from "./generated/skills.js";
+export { parseSkill } from "./parseSkill.js";
+export { scoreSkill } from "./scoreSkill.js";
